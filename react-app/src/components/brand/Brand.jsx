1490bdebd2ee6410783934b87bb3b068e25f58aa
Dilshod Akhmedov <akhmedov.20092004@gmail.com>
2022-12-02 20:53:27 +0500

    GPT-3

---
+++ react-app/src/components/brand/Brand.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./brand.css";
+import { google, slack, atlassian, shopify, dropbox } from "./imports";
+
+const Brand = () => {
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="Google" />
+      </div>
+      <div>
+        <img src={slack} alt="Slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="Atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="Dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="Shopify" />
+      </div>
+    </div>
+  );
+};
+
+export default Brand;
---
+++ react-app/src/components/brand/imports.js
@@ -0,0 +1,6 @@
+import google from "../../assets/google.png";
+import shopify from "../../assets/shopify.png";
+import slack from "../../assets/slack.png";
+import dropbox from "../../assets/dropbox.png";
+import atlassian from "../../assets/atlassian.png";
+export { google, shopify, slack, dropbox, atlassian };
